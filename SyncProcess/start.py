a9dfc27577067329411d9d17d4e7db4575acc89f
Bulat <khafizov.dds@yandex.ru>
2025-01-20 22:39:50 +0300

    added start.py, updated README.md

---
+++ SyncProcess/start.py
@@ -0,0 +1,54 @@
+from matplotlib import pyplot as plt
+
+from Enums.TpmType import TpmType
+from SyncProcess.sync_process import sync_process
+from SyncProcess.sync_process_with_queries import sync_process_with_queries
+from UpdateRules.update_rules import UpdateRules
+from Utils.utils import get_tpm_params
+
+
+def start(tpm_type, K, N, L, eve_attacks=False, plot_results=False, H=None, M=None, upd_rule=None):
+    use_queries, use_binary_inputs = get_tpm_params(tpm_type)
+
+    if not use_queries and upd_rule is None:
+        raise Exception('Default mode is chosen, but upd_rule value is None')
+    if use_queries and H is None:
+        raise Exception('Queries inputs mode is chosen, but H value is None')
+    if use_binary_inputs is False and M is None:
+        raise Exception('Binary inputs mode is chosen, but M value is None')
+
+    if use_queries:
+        result, eve_result, sync_weights, time_taken = sync_process_with_queries(K, N, L, H, eve_attacks=eve_attacks,
+                                                                                 use_binary_inputs=use_binary_inputs,
+                                                                                 M=M)
+    else:
+        result, eve_result, sync_weights, time_taken = sync_process(K, N, L, upd_rule, eve_attacks=eve_attacks,
+                                                                    use_binary_inputs=use_binary_inputs, M=M)
+
+    print(f"{TpmType(tpm_type).name}")
+    print(f"Параметры ДМЧ: K = {K}, N = {N}, L = {L}")
+    if not use_queries:
+        print(f"Для обновления весов используется правило {UpdateRules(upd_rule).name}")
+    else:
+        print(f'H = {H}')
+    if not use_binary_inputs:
+        print(f'M = {M}')
+    print(f"Вариаций весов: {2 * L + 1} ^ {K * N}")
+    print(f'Времени затрачено = {time_taken} секунд, итераций {result["nb_updates"]}')
+
+    plt.figure()
+    plt.plot(result["sync_history"], c='green')
+    plt.title('Динамика оценки синхронизации весовых коэффициентов двух ДМЧ')
+    plt.xlabel('Количество итераций')
+    plt.ylabel('Оценка')
+
+    if eve_attacks:
+        if eve_result["eve_score"] > 100:
+            print("Ева скомпрометировала ключ!")
+        else:
+            print(
+                f"Машина Евы синхронизирована на {eve_result['eve_score']}% по отношению к Алисе и Бобу, она совершила {eve_result['nb_eve_updates']} обновлений весов")
+            plt.plot(eve_result['eve_sync_history'], c='red')
+
+    if plot_results:
+        plt.show()
--- main.py
+++ main.py
@@ -1,53 +1,22 @@
-import matplotlib.pyplot as plt
 from Enums.TpmType import TpmType
+from SyncProcess.start import start
 from UpdateRules.update_rules import UpdateRules
-from SyncProcess.sync_process import sync_process
-from SyncProcess.sync_process_with_queries import sync_process_with_queries
-from Utils.utils import get_tpm_params
 
 # основные параметры ДМЧ
-K, N = 10, 100
-L = 7
+# K, N = 10, 100
+# L = 7
+
+K, N = 4, 5
+L = 5
 
 # доп параметры для вариаций алгоритма
-H = 3  # размер локального поля
-M = L  # численный порог значений входного вектора
+H = 2.5  # размер локального поля
+M = 7  # численный порог значений входного вектора
 
 
-tpm_type = TpmType.DefaultNonBinary  # QueriesNonBinary DefaultNonBinary
-eve_attacks = True
+tpm_type = TpmType.QueriesNonBinary  # QueriesNonBinary (мой), DefaultNonBinary, QueriesBinary, DefaultBinary
+eve_attacks = False
 upd_rule = UpdateRules.Hebbian
 plot_results = False
 
-use_queries, use_binary_inputs = get_tpm_params(tpm_type)
-if use_queries:
-    result, eve_result, sync_weights, time_taken = sync_process_with_queries(K, N, L, H, eve_attacks=eve_attacks, use_binary_inputs=use_binary_inputs, M=M)
-else:
-    result, eve_result, sync_weights, time_taken = sync_process(K, N, L, upd_rule, eve_attacks=eve_attacks, use_binary_inputs=use_binary_inputs, M=M)
-
-print(f"{TpmType(tpm_type).name}")
-print(f"Параметры ДМЧ: K = {K}, N = {N}, L = {L}")
-if not use_queries:
-    print(f"Для обновления весов используется правило {UpdateRules(upd_rule).name}")
-else:
-    print(f'H = {H}')
-if not use_binary_inputs:
-    print(f'M = {M}')
-print(f"Вариаций весов: {2 * L + 1} ^ {K * N}")
-print(f'Времени затрачено = {time_taken} секунд, итераций {result["nb_updates"]}')
-
-plt.figure()
-plt.plot(result["sync_history"], c='green')
-plt.title('Динамика оценки синхронизации весовых коэффициентов двух ДМЧ')
-plt.xlabel('Количество итераций')
-plt.ylabel('Оценка')
-
-if eve_attacks:
-    if eve_result["eve_score"] > 100:
-        print("Ева скомпрометировала ключ!")
-    else:
-        print(f"Машина Евы синхронизирована на {eve_result['eve_score']}% по отношению к Алисе и Бобу, она совершила {eve_result['nb_eve_updates']} обновлений весов")
-        plt.plot(eve_result['eve_sync_history'], c='red')
-
-if plot_results:
-    plt.show()
+start(tpm_type=tpm_type, K=K, N=N, L=L, eve_attacks=eve_attacks, plot_results=plot_results, H=H, M=M, upd_rule=upd_rule)
